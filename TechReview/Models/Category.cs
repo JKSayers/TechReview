f163c4b97c990560e534636477c6e075eecb395d
JKSayers <jsayers1980@gmail.com>
2017-02-13 06:11:43 -0500

    Final Commit

--- TechReview/Models/Category.cs
+++ TechReview/Models/Category.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,11 @@ namespace TechReview.Models
 {
     public class Category
     {
+        // Main key of Category model/foreign key of Review model
+        public int CategoryID { get; set; }
+
+        [Display(Name = "Tech type:")]
+        public string Name { get; set; }
+        public virtual ICollection<Review> Reviews { get; set; }
     }
 }
--- TechReview/Models/Review.cs
+++ TechReview/Models/Review.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,23 @@ namespace TechReview.Models
 {
     public class Review
     {
+
+        [Key] //Primary Key for Review table
+
+        public int ID { get; set; }
+
+        [Display(Name = "Reviewed by")]
+        public string ReviewerName { get; set; }
+        public string Title { get; set; }
+        [Display(Name = "Review")]
+        public string Content { get; set; }
+        [Display(Name = "Posted on")]
+        public DateTime PublishDate { get; set; }
+
+
+        [ForeignKey("Category")]  // Foreign Key connecting to Category table
+        public int CategoryID { get; set; }
+        public virtual Category Category { get; set; }
+
     }
 }
